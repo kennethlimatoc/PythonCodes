79c80aeb65ab8db4d4f21b242d4899993261ac76
kennethlimatoc <kenlimatoc17@gmail.com>
2025-03-24 00:17:12 +0800

    The Basic i learnead in python

---
+++ activity1.py
@@ -0,0 +1,27 @@
+# Student Information Sheet
+
+# Name of a student
+Name = "Alice"
+
+# For subject
+Subject = "Mathematics"
+
+# For day
+Day = "Monday to Friday"
+
+# For time
+Time = "7:00 AM - 8:30 AM"
+
+# For teacher
+Teacher = "Teacher Bob"
+
+#print the out put of the program
+print("\n")  
+print(f"Student Name: {Name}")
+print("\n")  
+print(f"Subject: {Subject}")
+print("\n")  
+print(f"Schedule: {Day}, {Time}")
+print("\n")  
+print(f"Teacher: {Teacher}")
+print("\n")  
